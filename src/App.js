87901f0c08e136b68c049b7040f04d3540d0f369
Smarsden007 <seannmarsden@gmail.com>
2022-09-29 22:22:23 -0700

    Working on new service tab

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import GallerySection from "./components/GallerySection";
 import { HeroSection } from "./components/HeroSection";
 import { OurBouncers } from "./components/OurBouncers";
 import { ServiceSection } from "./components/ServiceSection";
+import SimpleTabs from "./components/ServiceTabs";
 
 function App() {
   return (
     <div className="App">
      <HeroSection />
+     <SimpleTabs />
      <OurBouncers />
      <ServiceSection/>
      <Details />
---
+++ src/components/Tab.js
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+// import "./index.css";
+
+const Tab = ({ children, active = 0 }) => {
+  const [activeTab, setActiveTab] = useState(active);
+  const [tabsData, setTabsData] = useState([]);
+
+
+ 
+  useEffect(() => {
+    let data = [];
+
+    React.Children.forEach(children, (element) => {
+      if (!React.isValidElement(element)) return;
+
+      const {
+        props: { tab, children },
+      } = element;
+      data.push({ tab, children });
+    });
+
+    setTabsData(data);
+  }, [children]);
+
+  return (
+    <TabMasterDiv>
+        
+      <ul className="nav nav-tabs">
+        {tabsData.map(({ tab }, idx) => (
+            
+          <li className="nav-item">
+            <a
+              className={`nav-link ${idx === activeTab ? "active" : ""}`}
+              href="test"
+              onClick={() => setActiveTab(idx)}
+            >
+              {tab}
+            </a>
+          </li>
+          
+        ))}
+      </ul>
+      
+
+      <div className="tab-content p-3">
+        {tabsData[activeTab] && tabsData[activeTab].children}
+      </div>
+    </TabMasterDiv>
+  );
+};
+
+const TabPane = ({ children }) => {
+  return { children };
+};
+
+Tab.TabPane = TabPane;
+
+export default Tab;
+
+export const TabMasterDiv = styled.div`
+display: flex;
+flex-direction: column;
+background-color: #E6BEAE;
+`
